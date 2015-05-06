6310f6d0884b67709a1aec2662dbe240e2a915bc
jmibarrad <jmibarra@unitec.edu>
2015-05-06 11:45:32 -0600

    Design-Patterns Fooling

--- ProjectA/kernel.c
+++ ProjectA/kernel.c
@@ -9,26 +9,39 @@ void main(){
 	int x, y, i;
 	char* text = "Hello World!";
 
-	for(x=0; x < 30; x = x+1){
-		for(y=0; y < RMVA; y = y+1){
-			putInMemory(BASE_ADDRESS, memVideoAddr+(y)+(x*RMVA),0x00);
-			putInMemory(BASE_ADDRESS, memVideoAddr+(y+1)+(x*RMVA), 0x0);
+	for(y=0; y < 25; y = y+1){
+		for(x=0; x < RMVA; x = x+1){
+			putInMemory(BASE_ADDRESS, memVideoAddr+(x)+(y*RMVA),0x00);
+			putInMemory(BASE_ADDRESS, memVideoAddr+(x+1)+(y*RMVA), 0x0);
 		}
 	}
 
-	for(x=0; x < 30; x = x+1){
-		for(y=0; y < RMVA; y = y+1){
-			if(y==0 || y==40 || y==80 || y==120 || y == 158){
-			putInMemory(BASE_ADDRESS, memVideoAddr+(y)+(x*RMVA), 0x221);
-			putInMemory(BASE_ADDRESS, memVideoAddr+(y+1)+(x*RMVA), 0x34);
-			}		
+	for(y=0; y < 25; y = y+1){
+		for(x=0; x < RMVA; x = x+1){
+
+			if((x<30 || x>125) && y<21)
+				if((x%4==0) && (y%2==0)){
+				putInMemory(BASE_ADDRESS, memVideoAddr+(x)+(y*RMVA), 0x212);
+				putInMemory(BASE_ADDRESS, memVideoAddr+(x+1)+(y*RMVA), 0x74);
+				}else if((x%8==0)){
+				putInMemory(BASE_ADDRESS, memVideoAddr+(x)+(y*RMVA), 0x212);
+				putInMemory(BASE_ADDRESS, memVideoAddr+(x+1)+(y*RMVA), 0x74);
+				}
+			if(y>=21)
+				if((x%4==0) && (y%2==0)){
+				putInMemory(BASE_ADDRESS, memVideoAddr+(x)+(y*RMVA), 0x212);
+				putInMemory(BASE_ADDRESS, memVideoAddr+(x+1)+(y*RMVA), 0x71);
+				}else if((x%8==0)){
+				putInMemory(BASE_ADDRESS, memVideoAddr+(x)+(y*RMVA), 0x212);
+				putInMemory(BASE_ADDRESS, memVideoAddr+(x+1)+(y*RMVA), 0x71);
+				}
 		}
 	}
 
 	for(x=0; x < COLOR_ARRAY; x = x+1){
 		for(i=0; i < ARR_LENGTH; i = i+1){
-			putInMemory(BASE_ADDRESS, memVideoAddr+10+(x*RMVA), text[i]);
-			putInMemory(BASE_ADDRESS, memVideoAddr+11+(x*RMVA), i+1);
+			putInMemory(BASE_ADDRESS, memVideoAddr+70+(x*RMVA), text[i]);
+			putInMemory(BASE_ADDRESS, memVideoAddr+71+(x*RMVA), i+3);
 			memVideoAddr+=2;		
 		}
 		memVideoAddr-=(ARR_LENGTH*2);	
